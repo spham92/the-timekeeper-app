db2bbb1481fd4398570b658a2ef1e1df94fbc6f2
stpham <stpham@linkedin.com>
2021-08-23 00:00:07 -0700

    refactor: global state management

--- static/js/app.js
+++ static/js/app.js
@@ -1,42 +1,36 @@
 const DEFAULT_DURATION_MINUTES = 5;
-
-/**
- * There are 2 flows here. First is checking if the type was already
- * set to `CUSTOM` in previous interactions. If it was not, determined
- * the value by figuring out whether is not null or undefined or if it
- * does not match the default
- *
- * @param {number || undefined || null} queryParamDuration
- */
-function initializeDurationType(queryParamDuration) {
-    const storedDurationType = window.sessionStorage.getItem('durationType');
-
-    // If CUSTOM has been set in previous interactions, continue to use CUSTOM
-    if (storedDurationType && storedDurationType !== 'DEFAULT') {
-        return 'CUSTOM';
-    }
-
-    if (queryParamDuration === undefined || queryParamDuration === null) {
-        return 'DEFAULT';
-    }
-
-    if (queryParamDuration !== DEFAULT_DURATION_MINUTES) {
-        return 'CUSTOM';
-    } else {
-        return 'DEFAULT';
-    }
-}
+const LOCAL_STORAGE_KEY = 'timeKeeperState';
 
 class TimeKeeperApp {
     /**
      * Initialize the instance with the given values
-     *
-     * @param {number || undefined} duration - Integer in minutes
      */
-    constructor(duration) {
+    constructor() {
         this.durationChangeCallbacks = [];
-        this.updateTimekeeperState(duration || DEFAULT_DURATION_MINUTES, initializeDurationType(duration));
+        this.initializeState();
+        this.compareToUrlQueryParams();
         this.updateUrl();
+        this.setupLocalStorageChangeListener();
+    }
+
+    initializeState() {
+        const previousState = window.localStorage.getItem(LOCAL_STORAGE_KEY);
+
+        if (previousState) {
+            this.syncFromStateObject(JSON.parse(previousState));
+        } else {
+            this.updateTimekeeperState(DEFAULT_DURATION_MINUTES, 'DEFAULT');
+        }
+    }
+
+    compareToUrlQueryParams() {
+        const { duration: durationUrlQueryParam } = window.timeKeeperUtils.parseUrlQueryParams();
+
+        if (durationUrlQueryParam && durationUrlQueryParam !== this.duration) {
+            this.updateTimekeeperState(durationUrlQueryParam, 'CUSTOM');
+        } else {
+            this.updateTimekeeperState(this.duration, this.durationType);
+        }
     }
 
     /**
@@ -48,6 +42,12 @@ class TimeKeeperApp {
         url.searchParams.set('endTime', this.endTime);
         url.searchParams.set('startTime', this.startTime);
         window.history.pushState({}, '', url);
+    }
+
+    executeCallbacksForDurationChange() {
+        this.durationChangeCallbacks.forEach((callback) => {
+            callback(this.duration, this.startTime, this.endTime, this.durationType);
+        });
     }
 
     /**
@@ -62,22 +62,51 @@ class TimeKeeperApp {
         this.durationType = durationType;
         this.endTime = moment().unix();
         this.startTime = this.endTime - (this.duration * window.timeKeeperUtils.CONSTANTS.MINUTES_TO_MILLISECONDS);
-        this.updateUrl();
         this.persistToStorage();
 
-        this.durationChangeCallbacks.forEach((callback) => {
-            callback(this.duration, this.startTime, this.endTime);
+        this.executeCallbacksForDurationChange();
+    }
+
+
+    /**
+     * @param {{durationType: string, duration: number, startTime: number, endTime: number}} state
+     */
+    syncFromStateObject(state) {
+        this.durationType = state.durationType;
+        this.duration = state.duration;
+        this.endTime = state.endTime;
+        this.startTime = state.startTime;
+    }
+
+    setupLocalStorageChangeListener() {
+        window.addEventListener('storage', (event) => {
+            // Do nothing if the change is not for 'timeKeeperState' property
+            if (event.key !== LOCAL_STORAGE_KEY || !event.newValue) {
+                return;
+            }
+
+            this.syncFromStateObject(JSON.parse(event.newValue));
+            this.executeCallbacksForDurationChange();
+            this.updateUrl();
         });
     }
 
+    /**
+     * Store JSON string of the object of the current values for duration, durationType, endTime, and startTime
+     */
     persistToStorage() {
-        window.sessionStorage.setItem('durationType', this.durationType);
+        window.localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify({
+            duration: this.duration,
+            durationType: this.durationType,
+            endTime: this.endTime,
+            startTime: this.startTime,
+        }));
     }
 
     /**
      * Add the callback function to `durationChangeCallbacks`
      *
-     * @param {function} callback
+     * @param {function} callback - This function will be called with `duration`, `startTime`, `endTime`, `durationType`
      */
     callbackIfDurationChanged(callback) {
         this.durationChangeCallbacks.push(callback);
